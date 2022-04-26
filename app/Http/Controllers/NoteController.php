e17afab30777a08cf209873ccd4311df64e2d40b
Badru <badruakfm@gmail.com>
2022-04-26 14:25:48 +0700

    first init

---
+++ app/Http/Controllers/NoteController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Note;
+use Illuminate\Http\Request;
+
+class NoteController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $data = Note::all();
+        return response()->json([
+            'data' => [
+                'notes' => $data
+            ]
+        ]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        Note::create($request->all());
+
+        return response()->json(['message' => 'Data saved']);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        Note::where('id', $id)->update($request->all());
+
+        return response()->json(['message' => 'Data updated']);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        Note::destroy($id);
+
+        return response()->json(['message' => 'Data deleted']);
+    }
+}
